340dc383c2428f417f20ff4c6f937121663c54ff
danielhoerner <daniel.hoerner@vyaire.com>
2020-06-22 16:33:14 +0200

    - Add new function to get repository sources for a specific node (commit) and subpath - Add new function to get content of a file in repository

--- src/Bitbucket.Cloud.Net/v2/Repositories/Src/BitbucketCloudClient.cs
+++ src/Bitbucket.Cloud.Net/v2/Repositories/Src/BitbucketCloudClient.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 using System.Threading.Tasks;
 using Bitbucket.Cloud.Net.Common.Models;
 using Bitbucket.Cloud.Net.Models.v2;
@@ -12,13 +13,32 @@ namespace Bitbucket.Cloud.Net
 		private IFlurlRequest GetSourceUrl(string workspaceId, string repositorySlug, SourceFormats? format = null) => GetBaseUrl($"2.0/repositories/{workspaceId}/{repositorySlug}/src")
 			.SetQueryParam("format", format);
 
-		public async Task<IEnumerable<Source>> GetRepositorySourceAsync(string workspaceId, string repositorySlug, SourceFormats? format = null, int? maxPages = null)
-		{
+        private IFlurlRequest GetSourceUrl(string workspaceId, string repositorySlug, string node, string path, SourceFormats? format = null) => GetBaseUrl($"2.0/repositories/{workspaceId}/{repositorySlug}/src/{node}/{path}")
+            .SetQueryParam("format", format);
+
+    public async Task<IEnumerable<Source>> GetRepositorySourceAsync(string workspaceId, string repositorySlug, SourceFormats? format = null, int? maxPages = null)
+	{
 			return await GetPagedResultsAsync(maxPages, GetSourceUrl(workspaceId, repositorySlug, format), async req =>
 					await req
 						.GetJsonAsync<PagedResults<Source>>()
 						.ConfigureAwait(false))
 				.ConfigureAwait(false);
-		}
 	}
+
+    public async Task<IEnumerable<Source>> GetRepositorySourceAsync(string workspaceId, string repositorySlug, string node, string path, SourceFormats? format = null, int? maxPages = null)
+    {
+      return await GetPagedResultsAsync(maxPages, GetSourceUrl(workspaceId, repositorySlug, node, path, format), async req =>
+              await req
+                  .GetJsonAsync<PagedResults<Source>>()
+                  .ConfigureAwait(false))
+          .ConfigureAwait(false);
+    }
+
+    public async Task<Stream> ReadRepositorySourceAsync(string workspaceId, string repositorySlug, string node, string path)
+      {
+        return await GetSourceUrl(workspaceId, repositorySlug, node, path).
+            GetStreamAsync()
+            .ConfigureAwait(false);
+      }
+  }
 }
